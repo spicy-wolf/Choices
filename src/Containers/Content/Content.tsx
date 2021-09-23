485e02e4e3a8dbcedd8796ff3c76cf03d242ae34
spicy-wolf <89831155+spicy-wolf@users.noreply.github.com>
2021-09-22 21:03:45 -0700

    implement lazy executing

--- src/Containers/Content/Content.tsx
+++ src/Containers/Content/Content.tsx
@@ -10,23 +10,26 @@ const Content = (props: ContentProps) => {
   const [executeMore, setExecuteMore] = React.useState<boolean>(false);
   const [readingLogs, setReadingLogs] = React.useState<StatementType[]>([]); // TODO: load history
 
-  const addReadingLogs = (statements: StatementType[]): void => {
-    let newLogs = [...readingLogs, ...statements];
-    setReadingLogs(newLogs);
-  };
+  const [distanceToBottom, setDistanceToBottom] = React.useState<number>(0);
+
+  const contentRef = React.useRef<HTMLDivElement>();
 
   React.useEffect(() => {
     if (!executeMore) {
-      // TODO: more features on this confition
-      if (statementCounter < props.scripts.length) {
+      // TODO: more features on this condition
+      // pre-load some. TODO: hard code 100?
+      if (statementCounter < props.scripts.length && distanceToBottom < 100) {
         setExecuteMore(true);
       }
     }
-  }, [executeMore]);
+  }, [executeMore, distanceToBottom]);
 
   React.useEffect(() => {
     if (!executeMore) return;
 
+    /**
+     * Execute one statement
+     */
     const currentScripts = props.scripts;
     RenderEngine.Statements.Executor(currentScripts[statementCounter], {
       addReadingLogs,
@@ -34,6 +37,15 @@ const Content = (props: ContentProps) => {
     setStatementCounter(statementCounter + 1);
     setExecuteMore(false);
   }, [executeMore]);
+
+  React.useEffect(() => {
+    if (!contentRef.current) return;
+
+    const element = contentRef.current;
+    const _distanceToBottom = getDistanceToBottom(element);
+    setDistanceToBottom(_distanceToBottom);
+    console.log(_distanceToBottom);
+  }, [readingLogs]);
 
   const theStory = React.useMemo(() => {
     const result = readingLogs.map((statement, index) => (
@@ -44,11 +56,35 @@ const Content = (props: ContentProps) => {
     return result;
   }, [readingLogs]);
 
+  const addReadingLogs = (statements: StatementType[]): void => {
+    let newLogs = [...readingLogs, ...statements];
+    setReadingLogs(newLogs);
+  };
+
+  const onScrollWrapper = (e: React.UIEvent<HTMLDivElement>) => {
+    let element = e.target as HTMLDivElement;
+    const _distanceToBottom = getDistanceToBottom(element);
+    setDistanceToBottom(_distanceToBottom);
+    console.log(_distanceToBottom);
+  };
+
   return (
     <div id="content">
-      <div id="contentBody">{theStory}</div>
+      <div id="contentBody" onScroll={onScrollWrapper} ref={contentRef}>
+        {theStory}
+      </div>
     </div>
   );
 };
 
 export default Content;
+
+//
+// helper functions
+//
+function getDistanceToBottom(element: HTMLElement): number {
+  if (!element) return 0;
+  const _distanceToBottom =
+    element.scrollHeight - element.scrollTop - element.clientHeight;
+  return _distanceToBottom;
+}
