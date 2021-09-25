62400ace52d51c4da8afda2761a275af645b91bc
Spicy Wolf <89831155+spicy-wolf@users.noreply.github.com>
2021-09-24 17:34:55 -0700

    refactor side panel control button (#9)

--- src/Containers/SidePanel/SidePanel.tsx
+++ src/Containers/SidePanel/SidePanel.tsx
@@ -1,14 +1,42 @@
 import React, { useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './SidePanel.scss';
+import 'bootstrap/scss/bootstrap.scss';
+import { IoGitBranch, IoSettingsOutline } from 'react-icons/io5';
+import SidePanelControlBtn from './SidePanelControlBtn/SidePanelControlBtn';
 
-type SidePanelProps = { isOpen: boolean };
+type SidePanelProps = {};
 
 const SidePanel = (props: SidePanelProps) => {
-  const sidePanelClassName = props.isOpen ? 'open' : 'closed';
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState<boolean>(false);
+  const toggleSidebar = (event: React.FormEvent<HTMLDivElement>) => {
+    setIsSidePanelOpen(!isSidePanelOpen);
+  };
+
+  const sidePanelClassName = isSidePanelOpen ? 'open' : 'closed';
 
   return (
     <div id="sidePanel" className={sidePanelClassName}>
-      <div id="sidePanelBody" className={sidePanelClassName}></div>
+      <div id="sidePanelBody" className={sidePanelClassName}>
+        <SidePanelControlBtn
+          isSidePanelOpen={isSidePanelOpen}
+          onClick={toggleSidebar}
+        />
+        <div className="h-100 p-1 d-flex flex-column" style={{ rowGap: '5px' }}>
+          <div className="border rounded flex-grow-1 bg-light"></div>
+          <div
+            className="d-flex align-items-center justify-content-around border rounded bg-light"
+            style={{ height: '50px' }}
+          >
+            <div>
+              <IoGitBranch size="2em" />
+            </div>
+            <div>
+              <IoSettingsOutline size="2em" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
