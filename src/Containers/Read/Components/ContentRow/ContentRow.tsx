3291ab8773fcc8644357eb43a261cc2b245b0ffa
Spicy Wolf <89831155+spicy-wolf@users.noreply.github.com>
2022-05-25 22:42:42 -0700

    Implement store read log pos (#31)
    implement #30
    
    also, refactor useReducer,
    if the value is the same as prev, then no updates

--- src/Containers/Read/Components/ContentRow/ContentRow.tsx
+++ src/Containers/Read/Components/ContentRow/ContentRow.tsx
@@ -7,18 +7,40 @@ type AnyStatementType = Types.Statements.AnyStatementType;
 const ContentRow = (props: {
   data: AnyStatementType[];
   index: number;
+  isScrolling: boolean;
   setItemSize: (index: number, height: number) => void;
+  setReadingLogCursorPos?: (topScreenItemId: string) => void;
 }): JSX.Element => {
   const rowRef = React.useRef<HTMLDivElement>();
 
   React.useEffect(() => {
     if (rowRef.current) {
-      props.setItemSize(
-        props.index,
-        rowRef.current.getBoundingClientRect().height
-      );
+      const domRect = rowRef.current.getBoundingClientRect();
+      props.setItemSize(props.index, domRect.height);
     }
   }, [props.data, props.setItemSize, props.index]);
+
+  React.useEffect(() => {
+    if (props.isScrolling) return; // still scrolling, wait unit scrolling stoped
+
+    // search from top of the dom
+    // until the one on the top of the screen
+    // TODO: useDebounce
+    if (rowRef.current) {
+      const domRect = rowRef.current.getBoundingClientRect();
+      if (domRect.top <= 0 && domRect.bottom >= 0) {
+        const childElements = rowRef.current.children;
+        for (const child of childElements) {
+          const childDomRect = child.getBoundingClientRect();
+          if (childDomRect.top <= 0 && childDomRect.bottom >= 0) {
+            props.setReadingLogCursorPos &&
+              props.setReadingLogCursorPos(child.id);
+            break;
+          }
+        }
+      }
+    }
+  }, [props.isScrolling]);
 
   const theStory = React.useMemo(() => {
     const result = props.data?.map((statement) => (
