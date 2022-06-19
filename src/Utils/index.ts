9c6976605efd88b9ef50709b99b012988b2c71b0
Spicy Wolf <89831155+spicy-wolf@users.noreply.github.com>
2022-06-19 14:07:11 -0700

    Add Debounce for writting to DB (#44)

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -4,10 +4,12 @@ import { generateColorFromStr } from './generateColorFromStr';
 import { combinePath } from './combinePath';
 import { digeststring } from './digeststring';
 import { useQuery } from './useQuery';
+import { useDebounce } from './useDebounce';
 import { propertyOf } from './propertyOf';
 
 export {
   useQuery,
+  useDebounce,
   combinePath,
   digeststring,
   generateColorFromStr,
