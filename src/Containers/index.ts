c6848333c9c3442393ca4859242fe5d29ebf3297
Spicy Wolf <89831155+spicy-wolf@users.noreply.github.com>
2022-12-29 23:53:46 -0800

    Rename 'Library' and 'Read' to 'LibraryPage' and 'ReadPage' (#78)

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -1,6 +1,6 @@
 import File2ScriptConverter from './File2ScriptConverter/File2ScriptConverter';
-import LibraryPage from './Library/Library';
-import ReadPage from './Read/Read';
+import LibraryPage from './LibraryPage/LibraryPage';
+import ReadPage from './ReadPage/ReadPage';
 import SettingPage from './SettingPage/SettingPage';
 
 export { LibraryPage, ReadPage, File2ScriptConverter, SettingPage };
