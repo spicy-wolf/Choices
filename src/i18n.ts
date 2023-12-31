d9ac52e13fd65a829d3807d56c06a8e9a5f82e87
spicy-wolf <89831155+spicy-wolf@users.noreply.github.com>
2023-12-30 17:18:20 -0800

    fix i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
+declare let __URL_BASE_ROOT__: string;
+
 i18n
   .use(Backend)
   .use(initReactI18next)
@@ -11,6 +13,9 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+    backend: {
+      loadPath: `${__URL_BASE_ROOT__}/locales/{{lng}}/translation.json`
+    }
   });
 
 export default i18n;
