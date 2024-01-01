a068b417a0c461687114d18e9f9515b5fe169eb3
spicy-wolf <89831155+spicy-wolf@users.noreply.github.com>
2024-01-01 14:24:37 -0800

    remove subdirectories support

--- src/i18n.ts
+++ src/i18n.ts
@@ -9,8 +9,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
-declare let __BASE_URL__: string;
-
 i18n
   .use(Backend)
   .use(initReactI18next)
@@ -20,9 +18,6 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    backend: {
-      loadPath: `${__BASE_URL__}/locales/{{lng}}/translation.json`
-    }
   });
 
 export default i18n;
